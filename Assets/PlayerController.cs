614abd28c8e4b069d7ebebf4216afe45a6b70b41
tbrachmann <tobybrachmann@gmail.com>
2016-10-07 12:12:16 -0700

    Update available moves

--- Assets/PlayerController.cs
+++ Assets/PlayerController.cs
@@ -60,43 +60,48 @@ public class PlayerController : MonoBehaviour {
     }
 
     //Use this to paint all available moves.
+    //Can we pre-bake this into the coming A* algorithm to make sure that it only selects from available moves?
     HashSet<Vector3> CalculateMoveLimits(Vector3 current, HashSet<Vector3> visited, int limit = playerMoveLimit) {
         if (limit == 0) {
             visited.Add(current);
             return visited;
         }
         visited.Add(current);
-        limit = limit - 1;
+        limit -= 1;
         //if right in visited - don't add else rightVect
         //Vector3 rightVect = new Vector3(current.x + 1, current.y, current.z);
         Vector3 rightVect = current + Vector3.right;
         HashSet<Vector3> rightVisited = new HashSet<Vector3> { };
+        rightVisited.UnionWith(visited);
         if (!(visited.Contains(rightVect))) {
-            rightVisited = CalculateMoveLimits(rightVect, new HashSet<Vector3> {current}, limit);
+            rightVisited = CalculateMoveLimits(rightVect, rightVisited, limit);
         }
         //if left in visited - don't add else leftVect
         //Vector3 leftVect = new Vector3(current.x, current.y, current.z + 1);
         Vector3 leftVect = current + Vector3.left;
         HashSet<Vector3> leftVisited = new HashSet<Vector3> { };
+        leftVisited.UnionWith(visited);
         if (!(visited.Contains(leftVect)))
         {
-            leftVisited = CalculateMoveLimits(leftVect, new HashSet<Vector3> {current}, limit);
+            leftVisited = CalculateMoveLimits(leftVect, leftVisited, limit);
         }
         //if up in visited - don't add else upVect
         //Vector3 upVect = new Vector3(current.x + 1, current.y, current.z + 1);
         Vector3 upVect = current + Vector3.forward;
         HashSet<Vector3> upVisited = new HashSet<Vector3> { };
+        upVisited.UnionWith(visited);
         if (!(visited.Contains(upVect)))
         {
-            upVisited = CalculateMoveLimits(upVect, new HashSet<Vector3> {current}, limit);
+            upVisited = CalculateMoveLimits(upVect, upVisited, limit);
         }
         //if down in visited - don't add else downVect
         //Vector3 downVect = new Vector3(current.x - 1, current.y, current.z - 1);
         Vector3 downVect = current + Vector3.back;
         HashSet<Vector3> downVisited = new HashSet<Vector3> { };
+        downVisited.UnionWith(visited);
         if (!(visited.Contains(downVect)))
         {
-            downVisited = CalculateMoveLimits(downVect, new HashSet<Vector3> {current}, limit);
+            downVisited = CalculateMoveLimits(downVect, downVisited, limit);
         }
         visited.UnionWith(upVisited);
         visited.UnionWith(downVisited);
