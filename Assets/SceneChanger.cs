fcaa2adb865988d918a7e7ade7e99ec6eb7d1877
unknown <skz.zxc@gmail.com>
2016-10-08 19:21:53 -0400

    basic code for menu screen. but may have compatibility issues?

---
+++ Assets/SceneChanger.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SceneChanger : MonoBehaviour {
+
+	public string levelToLoad;
+
+	public void StartButtonPress() {
+		Debug.Log ("pressed start button");
+		Application.LoadLevel (levelToLoad);
+	}
+
+	public void SettingsButtonPress() {
+		Debug.Log ("pressed settings button");
+		//Application.LoadLevel (levelToLoad);
+	}
+}
